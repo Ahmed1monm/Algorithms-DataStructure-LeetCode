7fd0d22265966240ec8d8eb2385e34eb84ed1a0c
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-09-01 23:00:19 +0300

    125 valid palindrome

---
+++ LeetCode/125. Valid Palindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(s: str) -> bool:
+    value = ""
+    for i in range(0 , len(s)):
+        if s[i].isalpha() or s[i].isnumeric():
+            value = value + s[i]
+        else:
+            continue
+    if value.lower() == value.lower()[::-1]:
+        return True
+    return False
